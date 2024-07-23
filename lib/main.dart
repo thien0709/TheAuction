98e49e0cbace0641030efc643cb2d618a1b59001
thien0709 <thien070904@gmail.com>
2024-07-23 09:36:38 +0700

    add home - search - login -wallet

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
+import 'home.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -24,26 +25,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-   loginbutton() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Login'),
-          content: const Text('Login Success'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
+void login() async {
+  HomeScreen();
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -83,12 +67,24 @@ class _MyHomePageState extends State<MyHomePage> {
             const SizedBox(height: 20,),
             ElevatedButton(
               onPressed: (){
-                loginbutton();
+                login();
                 },
               child: const Text('Login'),
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+          color: Color.fromARGB(233, 31, 31, 32),
+          animationCurve: Curves.fastLinearToSlowEaseIn,
+          backgroundColor: Colors.white,
+          items: const <Widget>[
+            Icon(FontAwesomeIcons.chartSimple, size: 30,color:  Color.fromARGB(255, 122, 122, 122)),
+            Icon(FontAwesomeIcons.magnifyingGlassDollar, size: 30,color:  Color.fromARGB(255, 122, 122, 122)),
+            Icon(FontAwesomeIcons.ethereum, size: 30,color: Color.fromARGB(255, 122, 122, 122)),
+            Icon(FontAwesomeIcons.wallet, size: 30,color: Color.fromARGB(255, 122, 122, 122)),
+            Icon(FontAwesomeIcons.gear, size: 30,color:  Color.fromARGB(255, 122, 122, 122)),
+          ],
       )
     );
   }
