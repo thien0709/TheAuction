e723877cca75cb523b6ac0b2f65ae45b098b4404
thien0709 <thien070904@gmail.com>
2024-09-02 07:47:56 +0700

    change login

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Login extends StatefulWidget {
   const Login({super.key});
 
@@ -10,38 +11,68 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.only(
-          left: 20,
-          right: 20,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Welcome to The Auction', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,fontFamily: 'Koulen'),),
-            const SizedBox(height: 20,),
-            const Text('Please login to continue', style: TextStyle(fontSize: 12,fontFamily: 'JetBrainsMono'),),
-            const SizedBox(height: 20,),
-            const TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Username',
+      padding: const EdgeInsets.only(
+        left: 20,
+        right: 20,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Welcome to The Auction',
+            style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Koulen'),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            'Please login to continue',
+            style: TextStyle(fontSize: 12, fontFamily: 'JetBrainsMono'),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                  onTap: () {
+                    print("Connect with MetaMask");
+                  },
+                  child: Image.asset('images/icons8-metamask-logo-48.png',
+                      width: 30)),
+              const SizedBox(
+                width: 20,
               ),
-            ),
-            const SizedBox(height: 20,),
-            const TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
+              GestureDetector(
+                  onTap: () {
+                    print("Connect with Github");
+                  },
+                  child: Image.asset('images/icons8-github-48.png', width: 30)),
+              const SizedBox(
+                width: 20,
               ),
-            ),
-            const SizedBox(height: 20,),
-            ElevatedButton(
-              onPressed: (){
-                },
-              child: const Text('Login'),
-            ),
-          ],
-        ),
+              GestureDetector(
+                  onTap: () {
+                    print("Connect with Google");
+                  },
+                  child: Image.asset('images/icons8-google-48.png', width: 30)),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: const Text('Sign up',
+                style: TextStyle(
+                    decoration: TextDecoration.underline, color: Colors.pink)),
+          ),
+        ],
+      ),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,14 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
+import google_sign_in_ios
 import path_provider_foundation
 import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
